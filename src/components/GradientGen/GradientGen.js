1cf225b8df58b7085bb537fbe6e872ae118ba070
NaveenSingh <naveensingh.mtp@gmail.com>
2024-05-21 19:43:29 +0530

    no message

--- src/components/GradientGen/GradientGen.js
+++ src/components/GradientGen/GradientGen.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from '../../../styles/Home.module.css';
 import { range } from '../../utils';
 import Spacer from '../Spacer/Spacer';
+import { X } from 'react-feather';
+import { animated } from 'react-spring';
+import useBoop from '../../../hooks/use-boop';
+
 
 function GradientGen() {
   const [colors, setColors] = React.useState([
@@ -11,14 +15,22 @@ function GradientGen() {
     '#000066',
     '#FF0040',
   ]);
-  const colorStops = colors.join(', ');
-  const backgroundImage = `linear-gradient(${colorStops})`
 
+  const [style, trigger] = useBoop({x: 7 });
+
+  const [numOfVisibleColors,setNumOfVisibleColors] = React.useState(4);
+  const visibleColors = colors.slice(0,numOfVisibleColors)
+
+  const colorStops = visibleColors.join(', ');
+  const backgroundImage = `linear-gradient(${colorStops})`
+  const Tag = false ? 'a' : 'button';
+  console.log('colors',colors);
   return (
     <>
+    
       <div className={styles.mainwrapper}>
         <div className={styles.rowleft}>
-          <h3>Colors : </h3>
+          <h3>Colors :  </h3> 
           <form>
             {colors.map((color,index) =>{
               const colorId = `color -${index}`;
@@ -26,14 +38,19 @@ function GradientGen() {
                 <input 
                       key={colorId}
                       id={colorId}
-                      type='color'
+                      type="color"
                       value={color}
                       className={styles.addcolor}
-                      onChange={setColors}
+                      onChange={event => {
+                        const newcolor = [...colors]
+                        newcolor[index] = event.target.value;
+                        setColors(newcolor);
+                      }}
                 />
               );
             })}
-          </form>
+          </form> 
+          <h3>Colors Mode : </h3>     
           {/* <h3>Colors Mode : </h3>
           <form>
             {colors.map((color,index) =>{
@@ -59,7 +76,27 @@ function GradientGen() {
                 backgroundImage,
               }}>
           </div>
-        </div>   
+        </div>
+        {/* <Tag href="#" onMouseEnter={trigger}>
+          Hover me
+          <animated.span style={style}>
+            <span className="viewarrow"> &nbsp; → </span>
+          </animated.span>
+        </Tag> */}
+        <div className={styles.testhover} style={{ display: 'flex',flexDirection: 'column',alignItems: 'end' }}>
+           
+            {/* <X className={styles.closeicon} size={36} />
+            <input type='color' className={styles.addcolor} onChange={setColors} /> */}
+             <animated.span style={style}>
+              <span className="viewarrow"> &nbsp; 
+              <X className={styles.closeicon} size={36} /> 
+                <input type='color' className={styles.addcolor} onChange={setColors}  onMouseEnter={trigger}  />
+              </span>
+            </animated.span>
+        </div>
+        <div>
+        <span className={styles.arrow}>→</span>
+          </div>   
       </div>
     </>
   );
