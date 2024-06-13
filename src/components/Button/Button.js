6e6249cf91e31eb3d763e36859cb97722ec7996c
NaveenSingh <naveensingh.mtp@gmail.com>
2024-06-13 23:48:52 +0530

    no message

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from "styled-components";
 
-function Button() {
-  return <div></div>;
+function Button({children}) {
+  return (
+    <Wrapper>
+      {children}
+    </Wrapper>
+  );
 }
 
 export default Button;
+
+const Wrapper = styled.button`
+  background-color: hotpink;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  height: 140px;
+  width: 120px;
+  cursor: pointer;
+`;
