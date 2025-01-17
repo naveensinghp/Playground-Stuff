e40e89b7ef0f35335d7b7a4c954d15a85d461e6b
NaveenSingh <naveensingh.mtp@gmail.com>
2025-01-17 10:03:23 +0530

    no message

---
+++ pages/filter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Modal from "../src/components/Modal";
+
+
+export default function Filter() {
+    const [isOpen, setIsOpen] = React.useState(false);
+    return(
+    <>
+    <h1>DD</h1>
+    <button
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Toggle Modal
+      </button>
+        <Modal
+        title="Example Modal"
+        isOpen={isOpen}
+        handleDismiss={() => setIsOpen(false)}
+      >
+        Hello World
+      </Modal>
+    </>
+    );
+}
